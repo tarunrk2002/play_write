3c18d5adb4aa453a3462c94ba6a9043c750c7ce0
Tarun rk <tarunrk2002@gmail.com>
2025-02-25 16:22:07 +0530

    searching

--- startt.py
+++ startt.py
@@ -1,9 +1,14 @@
 from playwright.sync_api import sync_playwright
 
 play = sync_playwright().start()
-Browser = play.webkit.launch()
+Browser = play.webkit.launch(headless=True)
 page = Browser.new_page()
-page.goto("https://www.youtube.com/")
-page.screenshot(path="google.png")
+page.goto("https://arxiv.org/search/")
+
+page.get_by_placeholder("Search term...").fill("machine learning")
+page.screenshot(path="ss.png")
+
+
+
 
 Browser.close()
