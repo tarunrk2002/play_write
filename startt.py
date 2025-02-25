e3356477c37e6fa7199cbcf92567a031d12a96ca
Tarun rk <tarunrk2002@gmail.com>
2025-02-25 19:44:25 +0530

    locator

--- startt.py
+++ startt.py
@@ -6,7 +6,27 @@ page = Browser.new_page()
 page.goto("https://arxiv.org/search/")
 
 page.get_by_placeholder("Search term...").fill("machine learning")
-page.screenshot(path="ss.png")
+page.get_by_role("button", name="Search").nth(1).click()
+# url = page.url
+# print(url)
+# print(page.content)
+
+
+page.screenshot(path="ss1.png")
+
+# page.locator("text=arXiv:2502.17437").click()
+
+
+
+# page.get_by_role("link", name="arXiv:2502.17437").click()
+
+
+
+
+page.screenshot(path="ss2.png")
+page.go_back()
+page.screenshot(path="ss3.png")
+page.close()
 
 
 
